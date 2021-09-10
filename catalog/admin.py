59a62cb2e901c6e1ef6e94ed2cf4e019234b7d2b
Dmitriy1001 <dumatkachuk@gmail.com>
2021-09-10 12:17:09 +0300

    added models to admin.py, updated imagefield in models, configured media settings

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
-from .models import Vacancy, Company, Specialty
+from .models import Vacancy, Company, Specialty, Application
 
 
-admin.site.register(Vacancy)
-admin.site.register(Company)
-admin.site.register(Specialty)
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'specialty', 'company', 'published_at')
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'employee_count', 'owner')
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('written_username', 'written_phone', 'vacancy', 'user')
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'code')
+    readonly_fields = ('title', 'code')
+
+
+
