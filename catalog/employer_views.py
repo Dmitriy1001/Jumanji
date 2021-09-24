6868715acbf95aec1358a3c13431f4e44fdccd7b
Dmitriy1001 <dumatkachuk@gmail.com>
2021-09-24 11:51:00 +0300

    split views into two files, job_seeker_views and employer_views

---
+++ catalog/employer_views.py
@@ -0,0 +1,134 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from django.db.models import Count
+from django.urls import reverse_lazy, reverse
+from django.utils import timezone
+from django.views.generic import CreateView, TemplateView, UpdateView, ListView
+
+from .forms import CompanyForm, VacancyForm
+from .mixins import HasCompanyMixin, HasNotCompanyMixin
+from .models import Vacancy, Company
+
+
+# EMPLOYER(MYCOMPANY) PART
+
+
+class MyCompanyLetsstart(LoginRequiredMixin, HasNotCompanyMixin, TemplateView):
+    template_name = 'catalog/employer/mycompany_letsstart.html'
+
+
+class MyCompanyCreate(LoginRequiredMixin, HasNotCompanyMixin, CreateView):
+    form_class = CompanyForm
+    template_name = 'catalog/employer/mycompany.html'
+    success_url = reverse_lazy('mycompany_update')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        messages.success(self.request, 'Компания создана')
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        msgs = [msg.message for msg in self.request._messages]
+        context['msg'] = msgs[0] if msgs else ''
+        context.update({'page': 'company', 'info': 'Создание компании'})
+        return context
+
+
+class MyCompanyUpdate(LoginRequiredMixin, HasCompanyMixin, UpdateView):
+    model = Company
+    form_class = CompanyForm
+    template_name = 'catalog/employer/mycompany.html'
+    success_url = reverse_lazy('mycompany_update')
+
+    def get_object(self, queryset=None):
+        if self.request.method == 'POST':
+            messages.success(self.request, 'Информация о компании обновлена')
+        return self.request.user.company
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        msgs = [msg.message for msg in self.request._messages]
+        context['msg'] = msgs[0] if msgs else ''
+        context.update({'page': 'company', 'info': 'Информация о компании'})
+        return context
+
+
+class MyCompanyVacancyList(LoginRequiredMixin, ListView):
+    model = Vacancy
+    template_name = 'catalog/employer/mycompany_vacancies_list.html'
+    context_object_name = 'vacancies'
+
+    def get_queryset(self):
+        self.kwargs['user'] = self.request.user
+        return (
+            Vacancy.objects.filter(company__owner=self.kwargs['user'])
+            .annotate(applications_count=Count('applications'))
+            .select_related('specialty', 'company')
+            .order_by('-published_at')
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            company = self.kwargs['user'].company
+        except User.company.RelatedObjectDoesNotExist:
+            company = None
+        context.update({'company': company, 'page': 'vacancies'})
+        return context
+
+
+class MyCompanyVacancyCreate(LoginRequiredMixin, HasCompanyMixin, CreateView):
+    form_class = VacancyForm
+    template_name = 'catalog/employer/mycompany_vacancy.html'
+
+    def form_valid(self, form):
+        form.instance.company = self.request.user.company
+        form.instance.published_at = timezone.now()
+        messages.success(self.request, 'Вакансия создана')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('mycompany_vacancy_update', args=[self.object.id])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        msgs = [msg.message for msg in self.request._messages]
+        context['msg'] = msgs[0] if msgs else ''
+        context.update({'page': 'vacancies', 'title': 'Создание вакансии'})
+        return context
+
+
+class MyCompanyVacancyUpdate(LoginRequiredMixin, UpdateView):
+    model = Vacancy
+    form_class = VacancyForm
+    template_name = 'catalog/employer/mycompany_vacancy.html'
+
+    def get_object(self, queryset=None):
+        vacancy_id = self.kwargs['vacancy_id']
+        try:
+            vacancy = (
+                Vacancy.objects.prefetch_related('applications')
+                .select_related('specialty')
+                .annotate(applications_count=Count('applications'))
+                .get(id=vacancy_id, company__owner=self.request.user)
+                # такая фильтрация нужна для того, чтобы нельзя было редактировать вакансии чужой компании
+            )
+        except Vacancy.DoesNotExist:
+            raise PermissionDenied('Вы можете редактировать вакансии только своей компании.')
+        if self.request.method == 'POST':
+            messages.success(self.request, 'Информация о вакансии обновлена')
+        return vacancy
+
+    def get_success_url(self):
+        return reverse('mycompany_vacancy_update', args=[self.object.id])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vacancy = self.object
+        msgs = [msg.message for msg in self.request._messages]
+        context['msg'] = msgs[0] if msgs else ''
+        context.update({'page': 'vacancies', 'title': vacancy.title})
+        return context
--- catalog/job_seeker_views.py
+++ catalog/job_seeker_views.py
@@ -1,18 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
-from django.core.exceptions import PermissionDenied
 from django.db.models import Count, Q
 from django.http import Http404
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
-from django.utils import timezone
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, TemplateView, UpdateView, ListView, DetailView
 
-from .forms import ApplicationForm, CompanyForm, VacancyForm, ResumeForm
-from .mixins import HasCompanyMixin, HasNotCompanyMixin, HasNotResumeMixin, HasResumeMixin
+from .forms import ApplicationForm, ResumeForm
+from .mixins import HasNotResumeMixin, HasResumeMixin
 from .models import Vacancy, Specialty, Company, Resume
 
 
@@ -135,7 +132,6 @@ class MyResumeLetsstart(LoginRequiredMixin, HasNotResumeMixin, TemplateView):
 class MyResumeCreate(LoginRequiredMixin, HasNotResumeMixin, CreateView):
     form_class = ResumeForm
     template_name = 'catalog/job_seeker/myresume.html'
-    extra_context = {'info': 'Создание резюме'}
     success_url = reverse_lazy('myresume_update')
 
     def form_valid(self, form):
@@ -147,6 +143,7 @@ class MyResumeCreate(LoginRequiredMixin, HasNotResumeMixin, CreateView):
         context = super().get_context_data(**kwargs)
         msgs = [msg.message for msg in self.request._messages]
         context['msg'] = msgs[0] if msgs else ''
+        context['info'] = 'Создание резюме'
         return context
 
 
@@ -166,125 +163,4 @@ class MyResumeUpdate(LoginRequiredMixin, HasResumeMixin, UpdateView):
         msgs = [msg.message for msg in self.request._messages]
         context['msg'] = msgs[0] if msgs else ''
         context['info'] = 'Мое резюме'
-        return context
-
-
-# EMPLOYER(MYCOMPANY) PART
-
-
-class MyCompanyLetsstart(LoginRequiredMixin, HasNotCompanyMixin, TemplateView):
-    template_name = 'catalog/employer/mycompany_letsstart.html'
-
-
-class MyCompanyCreate(LoginRequiredMixin, HasNotCompanyMixin, CreateView):
-    form_class = CompanyForm
-    template_name = 'catalog/employer/mycompany.html'
-    success_url = reverse_lazy('mycompany_update')
-
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        messages.success(self.request, 'Компания создана')
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        msgs = [msg.message for msg in self.request._messages]
-        context['msg'] = msgs[0] if msgs else ''
-        context.update({'page': 'company', 'info': 'Создание компании'})
-        return context
-
-
-class MyCompanyUpdate(LoginRequiredMixin, HasCompanyMixin, UpdateView):
-    model = Company
-    form_class = CompanyForm
-    template_name = 'catalog/employer/mycompany.html'
-    success_url = reverse_lazy('mycompany_update')
-
-    def get_object(self, queryset=None):
-        if self.request.method == 'POST':
-            messages.success(self.request, 'Информация о компании обновлена')
-        return self.request.user.company
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        msgs = [msg.message for msg in self.request._messages]
-        context['msg'] = msgs[0] if msgs else ''
-        context.update({'page': 'company', 'info': 'Информация о компании'})
-        return context
-
-
-class MyCompanyVacancyList(LoginRequiredMixin, ListView):
-    model = Vacancy
-    template_name = 'catalog/employer/mycompany_vacancies_list.html'
-    context_object_name = 'vacancies'
-
-    def get_queryset(self):
-        return (
-                Vacancy.objects.filter(company__owner=self.request.user)
-                .annotate(applications_count=Count('applications'))
-                .select_related('specialty', 'company')
-                .order_by('-published_at')
-        )
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        try:
-            company = self.request.user.company
-        except User.company.RelatedObjectDoesNotExist:
-            company = None
-        context.update({'company': company, 'page': 'vacancies'})
-        return context
-
-
-class MyCompanyVacancyCreate(LoginRequiredMixin, HasCompanyMixin, CreateView):
-    form_class = VacancyForm
-    template_name = 'catalog/employer/mycompany_vacancy.html'
-
-    def form_valid(self, form):
-        form.instance.company = self.request.user.company
-        form.instance.published_at = timezone.now()
-        messages.success(self.request, 'Вакансия создана')
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('mycompany_vacancy_update', args=[self.object.id])
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        msgs = [msg.message for msg in self.request._messages]
-        context['msg'] = msgs[0] if msgs else ''
-        context.update({'page': 'vacancies', 'title': 'Создание вакансии'})
-        return context
-
-
-class MyCompanyVacancyUpdate(LoginRequiredMixin, UpdateView):
-    model = Vacancy
-    form_class = VacancyForm
-    template_name = 'catalog/employer/mycompany_vacancy.html'
-
-    def get_object(self, queryset=None):
-        vacancy_id = self.kwargs['vacancy_id']
-        try:
-            vacancy = (
-                Vacancy.objects.prefetch_related('applications')
-                .select_related('specialty')
-                .annotate(applications_count=Count('applications'))
-                .get(id=vacancy_id, company__owner=self.request.user)
-                # такая фильтрация нужна для того, чтобы нельзя было редактировать вакансии чужой компании
-            )
-        except Vacancy.DoesNotExist:
-            raise PermissionDenied('Вы можете редактировать вакансии только своей компании.')
-        if self.request.method == 'POST':
-            messages.success(self.request, 'Информация о вакансии обновлена')
-        return vacancy
-
-    def get_success_url(self):
-        return reverse('mycompany_vacancy_update', args=[self.object.id])
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        vacancy = self.object
-        msgs = [msg.message for msg in self.request._messages]
-        context['msg'] = msgs[0] if msgs else ''
-        context.update({'page': 'vacancies', 'title': vacancy.title})
         return context
